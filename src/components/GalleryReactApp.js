0752ce022f4eb732ebafcbc73ecab9edc7522271
lizhuliang <2922520570@qq.com>
2018-04-01 20:12:33 +0800

    2th

--- src/components/GalleryReactApp.js
+++ src/components/GalleryReactApp.js
@@ -10,27 +10,52 @@ require('../styles/main.scss');
 //get image data
 var imageDatas = require('../data/imageDatas.json');
 
-// var imageURL = require('../images/yeoman.png');
+// 利用自执行函数， 将图片名信息转成图片URL路径信息
+imageDatas = (function genImageURL(imageDatasArr) {
+    for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+        var singleImageData = imageDatasArr[i];
 
-function getImageURL(imageDataArr) {
-  // body...
-  for (var i = 0, j = imageDataArr.length; i < j; i++){
-    var singleImageData = imageDataArr[i];
-    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
-    imageDataArr[i] = singleImageData;
-    return imageDataArr;
+        singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+        imageDatasArr[i] = singleImageData;
+    }
+
+    return imageDatasArr;
+})(imageDatas);
+
+var ImgFigure = React.createClass({
+  render: function(){
+    return (
+      <figure>
+        <img src={this.props.data.imageURL} alt={this.props.data.title}
+        />
+        <figcaption>
+          <h2>{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+      );
   }
-}
-
-imageDatas = getImageURL(imageDatas);
+});
 
 var GalleryReactApp = React.createClass({
   render: function() {
+
+    var controllerUnits = [],
+    imgFigures = [];
+
+    imageDatas.forEach(function(value){
+      imgFigures.push(<ImgFigure data={value}/>);
+    });
+
+
     return (
       <section className="stage">
         <section className="img-sec">
+          {imgFigures}
         </section>
-        <nav className="controller-nav"></nav>
+        <nav className="controller-nav">
+            {controllerUnits}
+        </nav>
 
       </section>
     );
